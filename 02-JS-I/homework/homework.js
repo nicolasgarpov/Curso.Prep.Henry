b24bced2738455c1ab5026fc20fb29a43a856487
nicolasgarpov <114888169+nicolasgarpov@users.noreply.github.com>
2023-01-12 16:07:06 -0500

    finish homework

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -160,7 +160,7 @@ function redondearHaciaArriba(num) {
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-  retrun Math.random();
+  return Math.random();
 }
 
 function esPositivo(numero) {
